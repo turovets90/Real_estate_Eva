b18374d7f1c6f4c1227221a8038bdd7ea558056b
Aleksandr <a.turovets90@gmail.com>
2021-03-10 00:24:54 +0300

    edit files

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
 $(document).ready(function(){
 
-    /* перевод картинки svg в код */
-    $('.icon img, img.icon').each(function(){
-        var $img = $(this);
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-        $.get(imgURL, function(data) {
-            var $svg = $(data).find('svg');
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
-            }
-            $svg = $svg.removeAttr('xmlns:a');
-            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-            }
-            $img.replaceWith($svg);
-        }, 'xml');
-    });
-    /* end перевод картинки svg в код */
-
-
     $('.hamburger').click(function () {
         $('.main_menu').toggleClass('open');
         $('html').toggleClass('page-noscroll');
